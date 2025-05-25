be92181110be6c50cc98da3e8549ef345f4cf017
kokitree <94977757+tharumbun@users.noreply.github.com>
2025-05-25 17:33:15 +0700

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PenhPixel'
+export const SITE_TITLE = 'CamboPixdia'
 export const SITE_DESCRIPTION = 'a photo blog by Cambopedia'
